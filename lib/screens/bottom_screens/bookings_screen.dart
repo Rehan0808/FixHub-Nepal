610d88d5ed78b5fb74976f76c840ca8b04c57c4c
Rehan Pradhan <rehanpradhan10@gmail.com>
2025-12-18 16:18:04 +0545

    created booking and tracking screen

---
+++ lib/screens/bottom_screens/bookings_screen.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class BookingsScreen extends StatelessWidget {
+  const BookingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('No Bookings Yet', style: TextStyle(fontSize: 18, color: Colors.grey)));
+  }
+}
