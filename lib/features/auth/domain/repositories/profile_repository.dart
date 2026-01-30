d747e72db69f14536af568801f39ce173af47acb
Rehan Pradhan <rehanpradhan10@gmail.com>
2026-01-30 16:48:13 +0545

    profiel repository  created

---
+++ lib/features/auth/domain/repositories/profile_repository.dart
@@ -0,0 +1,6 @@
+import 'package:image_picker/image_picker.dart';
+import '../entities/user_profile_entity.dart';
+
+abstract class ProfileRepository {
+  Future<UserProfileEntity> uploadProfileImage(XFile imagePath);
+}
