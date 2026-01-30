58eb399d049e6be062b67dd4b53fd51b761dbf6e
Rehan Pradhan <rehanpradhan10@gmail.com>
2026-01-30 16:43:02 +0545

    api endpoints file updated

--- lib/core/api/api_endpoints.dart
+++ lib/core/api/api_endpoints.dart
@@ -6,9 +6,20 @@
 //   static const String signup = '$baseUrl/auth/register';
 // }
 
+// class ApiEndpoints {
+//   static const String baseUrl = 'http://localhost:5000/api';
+
+//   // AUTH
+//   static const String login = '$baseUrl/auth/login';
+//   static const String signup = '$baseUrl/auth/register';
+// }
+
 class ApiEndpoints {
-  static const String baseUrl = 'http://192.168.1.97:5000/api';
+  // static const String baseUrl = 'http://192.168.1.97:5000/api';
+   static const String baseUrl = 'http://localhost:5000/api';
+  
 
   static const String login = '$baseUrl/auth/login';
   static const String signup = '$baseUrl/auth/register';
+  static const String uploadProfileImage = '$baseUrl/profile/upload';
 }
