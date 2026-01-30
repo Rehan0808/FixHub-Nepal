877763728b340d0cf17b0521686308d21d239609
Rehan Pradhan <rehanpradhan10@gmail.com>
2026-01-30 16:49:13 +0545

    profile image upload usecase  created

---
+++ lib/features/auth/domain/usecases/upload_profile_image_usecase.dart
@@ -0,0 +1,13 @@
+import 'package:image_picker/image_picker.dart';
+import '../entities/user_profile_entity.dart';
+import '../repositories/profile_repository.dart';
+
+class UploadProfileImageUseCase {
+  final ProfileRepository repository;
+
+  UploadProfileImageUseCase(this.repository);
+
+  Future<UserProfileEntity> execute(XFile imagePath) {
+    return repository.uploadProfileImage(imagePath);
+  }
+}
