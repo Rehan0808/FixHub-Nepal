a5a729a51942aa9f5001dc61cd877f1cf82367b9
Rehan Pradhan <rehanpradhan10@gmail.com>
2026-01-30 12:37:43 +0545

    auth repo created

--- lib/features/auth/data/repositories/auth_repository_impl.dart
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -1,29 +1,43 @@
 import '../../domain/entities/auth_entity.dart';
 import '../../domain/repositories/auth_repository.dart';
-import '../datasources/auth_hive_datasource.dart';
-import '../models/auth_hive_model.dart';
+import '../datasources/auth_remote_datasource.dart';
+import '../models/auth_model.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
-  final AuthHiveDataSource dataSource;
+  final AuthRemoteDataSource remoteDataSource;
 
-  AuthRepositoryImpl(this.dataSource);
+  AuthRepositoryImpl(this.remoteDataSource);
 
+  /// 🔹 SIGN UP USING API
   @override
   Future<void> signUp(AuthEntity user) async {
-    final model = AuthHiveModel(
+    final model = AuthModel(
       id: user.id,
       email: user.email,
       password: user.password,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      phone: user.phone,
+      address: user.address,
     );
-    await dataSource.addUser(model);
+
+    await remoteDataSource.signup(model);
   }
 
+  /// 🔹 LOGIN USING API
   @override
   Future<AuthEntity?> login(String email, String password) async {
-    final user = dataSource.getUser(email);
-    if (user != null && user.password == password) {
-      return AuthEntity(id: user.id, email: user.email, password: user.password);
-    }
-    return null;
+    final AuthModel userModel =
+        await remoteDataSource.login(email, password);
+
+    return AuthEntity(
+      id: userModel.id,
+      email: userModel.email,
+      password: password,
+      firstName: userModel.firstName,
+      lastName: userModel.lastName,
+      phone: userModel.phone,
+      address: userModel.address,
+    );
   }
 }
