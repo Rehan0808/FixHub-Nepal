2dd750ac92f699df7ed34d4e870cc9fa2cad4613
Rehan Pradhan <rehanpradhan10@gmail.com>
2026-03-08 23:19:55 +0545

    feat(auth): update domain repository contracts for auth and profile

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -2,5 +2,7 @@ import '../entities/auth_entity.dart';
 
 abstract class AuthRepository {
   Future<void> signUp(AuthEntity user);
-  Future<AuthEntity?> login(String email, String password);
+  Future<Map<String, dynamic>> login(String email, String password);
+  Future<void> sendResetOtp(String email);
+  Future<void> resetPasswordWithOtp(String email, String otp, String newPassword);
 }
--- lib/features/auth/domain/repositories/profile_repository.dart
+++ lib/features/auth/domain/repositories/profile_repository.dart
@@ -2,5 +2,5 @@ import 'package:image_picker/image_picker.dart';
 import '../entities/user_profile_entity.dart';
 
 abstract class ProfileRepository {
-  Future<UserProfileEntity> uploadProfileImage(XFile imagePath);
+  Future<UserProfileEntity> uploadProfileImage(XFile imagePath, String token);
 }
