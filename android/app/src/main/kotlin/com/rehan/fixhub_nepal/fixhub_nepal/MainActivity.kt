2dbcda1bbd4409d510a848a6ca79c474a57ee56d
Rehan Pradhan <rehanpradhan10@gmail.com>
2026-03-08 23:19:05 +0545

    chore(android): update build config, manifest and gradle settings

--- android/app/src/main/kotlin/com/rehan/fixhub_nepal/fixhub_nepal/MainActivity.kt
+++ android/app/src/main/kotlin/com/rehan/fixhub_nepal/fixhub_nepal/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.rehan.fixhub_nepal.fixhub_nepal
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
